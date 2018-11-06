e3e9a901a5883e13552d8f899684e43ce8b6b5d7
Camden Bickel <cam@bickel.io>
2018-11-06 17:50:20 -0500

    update logo

--- site/src/components/logo.tsx
+++ site/src/components/logo.tsx
@@ -4,11 +4,11 @@ import Link from 'components/link'
 
 import styles from './logo.module.scss'
 
-const Logo = ({ title }) => {
+const Logo = () => {
   return (
     <div className={styles.title}>
       <Link to="/" className={styles.text}>
-        {title}
+        Rulebook.io <span className={styles.emphasis}>FAQs</span>
       </Link>
     </div>
   )
