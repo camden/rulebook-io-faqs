d596bd34b1312d4172028c02892bbf00768a4753
Camden Bickel <cam@bickel.io>
2018-11-09 09:53:12 -0500

    add site info to url in discussion

--- site/src/components/link.tsx
+++ site/src/components/link.tsx
@@ -12,7 +12,18 @@ type LinkProps = {
 const Link: StatelessComponent<LinkProps> = props => {
   let classNamesMerged = `${styles.link} ${props.className}`
 
-  return <GatsbyLink {...props} className={classNamesMerged} />
+  const internal = /^\/(?!\/)/.test(props.to)
+
+  // Use Gatsby Link for internal links, and <a> for others
+  if (internal) {
+    return <GatsbyLink {...props} className={classNamesMerged} />
+  }
+
+  return (
+    <a href={props.to} {...props} className={classNamesMerged}>
+      {props.children}
+    </a>
+  )
 }
 
 export default Link
--- site/src/templates/faq-page.js
+++ site/src/templates/faq-page.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import parseDomain from 'parse-domain'
 import { graphql } from 'gatsby'
 
+import Link from 'components/link'
 import Layout from 'components/layout'
 import Breadcrumbs from 'components/breadcrumbs'
 
@@ -14,8 +16,25 @@ const FAQPage = ({ data, pageContext }) => {
       <Breadcrumbs faq={faq} />
       <h1>{faq.question}</h1>
       <p>{faq.answer}</p>
+      <p>
+        {faq.discussion.map(url => (
+          <Link to={url} key={url} target={'_blank'}>
+            {formatDiscussionLinkTitle(url)}
+          </Link>
+        ))}
+      </p>
     </Layout>
   )
+}
+
+const formatDiscussionLinkTitle = url => {
+  const domainInfo = parseDomain(url)
+  if (!domainInfo) {
+    return 'Discussion'
+  } else {
+    const site = `${domainInfo.domain}.${domainInfo.tld}`
+    return `Discussion (${site})`
+  }
 }
 
 export const query = graphql`
