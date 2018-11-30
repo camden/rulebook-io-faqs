88a363adf0e385b2fb08c54e0cd8f2884c7c6953
Camden Bickel <cam@bickel.io>
2018-11-30 14:57:08 -0500

    alpha sort

--- site/src/pages/index.js
+++ site/src/pages/index.js
@@ -3,10 +3,16 @@ import { graphql } from 'gatsby'
 import Layout from 'components/layout'
 import Link from 'components/link'
 
+const alphabeticalSort = (_a, _b) => {
+  const a = _a.node
+  const b = _b.node
+  return a.name.localeCompare(b.name)
+}
+
 const IndexPage = ({ data }) => (
   <Layout>
     <h2>FAQs:</h2>
-    {data.allGamesHJson.edges.map(({ node }) => {
+    {data.allGamesHJson.edges.sort(alphabeticalSort).map(({ node }) => {
       return (
         <div key={node.id}>
           <Link to={node.fields.slug}>{node.name}</Link>
