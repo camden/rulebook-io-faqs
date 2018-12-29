6817c61217873f2fd01b3eefde187d74643a19a0
Camden Bickel <cam@bickel.io>
2018-12-29 14:40:39 -0800

    fixed capitalization

--- site/src/utils/capitalize.ts
+++ site/src/utils/capitalize.ts
@@ -1,5 +1,12 @@
-const capitalize = (str: string) => {
+const capitalizeWord = (str: string) => {
   return str.charAt(0).toUpperCase() + str.slice(1)
+}
+
+const capitalize = (str: string) => {
+  return str
+    .split(' ')
+    .map(capitalizeWord)
+    .join(' ')
 }
 
 export default capitalize
