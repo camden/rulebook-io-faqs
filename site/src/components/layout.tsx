d7d940430b12178ec92e5627c1210eee05dc9f56
Camden Bickel <cam@bickel.io>
2019-01-03 11:28:01 -0800

    add wildcard to match url prefix

--- site/src/components/layout.tsx
+++ site/src/components/layout.tsx
@@ -17,14 +17,14 @@ type LayoutProps = {
 const ConditionalRedirect = () => {
   return (
     <>
-      <Match path="/rules/:game">
+      <Match path="*/rules/:game">
         {props =>
           props.match ? (
             <Redirect to={`/games/${props.match.game}/rules`} noThrow />
           ) : null
         }
       </Match>
-      <Match path="/browse">
+      <Match path="*/browse">
         {props => (props.match ? <Redirect to={`/`} noThrow /> : null)}
       </Match>
     </>
